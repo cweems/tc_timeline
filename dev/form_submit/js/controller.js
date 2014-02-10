926369124cfccbb6d1fef152f0bd1587dfd908ac
Charlie Weems <charlie.weems@gmail.com>
2014-02-09 22:36:16 -0500

    First working version of image search

--- dev/form_submit/js/controller.js
+++ dev/form_submit/js/controller.js
@@ -1,17 +1,28 @@
 'use strict';
-var formApp = angular.module('formApp', []);
+angular.module('formApp', []);
+
+var searchQuery = "exam";
 
 
-$("#imageSearchButton").click(function(){ //Execute search when user clicks button
-	var searchQuery = "internet technology"
+$( "#imageQuery" )
+  .keyup(function() {
+    searchQuery = $( this ).val();
+    $( "p" ).text( searchQuery );
+  })
+  .keyup();
+
+
+$("#imageSearchButton").click(function($scope){ //Execute search when user clicks button
+	
 	var cleanQuery = searchQuery.replace(/\s+/g, '+'); //Replace spaces in the query with +
 	var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=68dc576fbfc642f59fdbd1032a6c6475&tags="+cleanQuery+"&safe_search=1&per_page=20"; //URL for API call
+	console.log(cleanQuery);
 	var src;
 	$.getJSON(url + "&format=json&jsoncallback=?", function(data){
 	    $.each(data.photos.photo, function(i,item){ 
-	        src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
-	        $("<img class='flickrImage'/>").attr("src", src).appendTo("#images");
-	        if ( i === 19 ) return false;
+			src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
+			$("<img class='flickrImage'/>").attr("src", src).appendTo("#images");
+			if ( i === 19 ) return false;
 	    });
 	});
 });
